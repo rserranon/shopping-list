06bda7cf63ee4db979ad499843cc1f5c83601de0
rserranon <rserranon@gmail.com>
2021-09-09 15:57:52 -0500

    crate item

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Item } from 'src/app/models/item';
+import { ItemService } from 'src/app/services/item.service';
 
 @Component({
   selector: 'app-add-item',
@@ -13,7 +15,7 @@ export class AddItemComponent implements OnInit {
   price: number = 0;
   quantity:number = 0;
 
-  constructor() { }
+  constructor( private itemService: ItemService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -25,6 +27,8 @@ export class AddItemComponent implements OnInit {
     item.price = this.price;
     item.quantity = this.quantity;
 
+    this.itemService.addItem(item);
+    this.router.navigate(['/']);
   }
 
 }
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Item } from '../models/item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
+  items: Item[] = [
+    {id: 0,
+      title: 'Manzana',
+      price: 20,
+      quantity: 2,
+      completed: false
+    },
+    {id: 1,
+      title: 'Pera',
+      price: 30,
+      quantity: 3,
+      completed: true
+    }
+  ];
+  
   constructor() { }
 
   getItems(){
-    return [
-      {id: 0,
-        title: 'Manzana',
-        price: 20,
-        quantity: 2,
-        completed: false
-      },
-      {id: 1,
-        title: 'Pera',
-        price: 30,
-        quantity: 3,
-        completed: true
-      }
-    ];
+    return this.items;
+  }
+
+  addItem ( item: Item){
+    this.items.unshift(item);
   }
 }
